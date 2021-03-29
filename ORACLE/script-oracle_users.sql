a2f125d901365c782ac35ad58f09df16b4351ec1
Gabriel Zoneti Figueira Peres <gabriel.zoneti@gmail.com>
2021-03-29 20:01:12 -0300

    Update script-oracle_users.sql

--- ORACLE/script-oracle_users.sql
+++ ORACLE/script-oracle_users.sql
@@ -1,6 +1,21 @@
-BEGIN  
-    FOR r IN (select sid,serial# from v$session where username = 'TEST_ANB')  LOOP  
-        EXECUTE IMMEDIATE 'alter system kill session ''' || r.sid  || ',' || r.serial# || '''';  
-    END LOOP;  
-END;  
-/
+/*SYNTAX :  create user <USER_NAME> identified by <PASSWORD>  default tablespace  <TABLESPACE_NAME>  temporary tablespace <TEMP_TABLESPACE>;
+*/
+create user SCOTT identified by oracle#41234  default tablespace users  temporary tablespace TEMP;
+
+/*
+  To create an user, which will prompt for new password upon login:  
+*/  
+create user SCOTT identified by oracle#41234  default tablespace users  temporary tablespace TEMP  password expire;
+
+/* Change password of an user  */
+ALTER USER SCOTT identified by NEW_PWD;  
+/* Change user profile;  */
+ALTER USER SCOTT PROFILE SIEBEL_PROFILE;  
+/* Unlock/lock a user  */
+ALTER USER SCOTT account unlock;  
+ALTER USER SCOTT account lock;  
+/* Make sure password expiry, so upon login, it will ask for new one  */
+ALTER USER SCOTT password expire;
+
+/*Change default tablespace of user*/
+ALTER USER SCOTT DEFAULT TABLESPACE DATATS;  
